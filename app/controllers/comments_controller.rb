a55c1d81a90c219ea8d78703837c45a79bf5003d
Susumu <sumura80@gmail.com>
2018-10-07 22:15:13 +0900

    Arrange show and comments page with css

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
 	before_action :find_post
 	before_action :find_comment ,only:[:show, :edit, :update, :destroy]
+	before_action :authenticate_user!
 
 	def create
 		@comment = @post.comments.create(comment_params)
