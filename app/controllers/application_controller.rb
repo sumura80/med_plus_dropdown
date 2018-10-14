101bf62d9900113ded5e5c0bbb6b2212fa3a5243
Susumu <sumura80@gmail.com>
2018-10-14 21:27:17 +0900

    ADD message in account page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@comments = Comment.order("created_at DESC")
 	end
 
 	def edit
