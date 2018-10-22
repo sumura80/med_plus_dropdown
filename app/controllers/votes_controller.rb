6d8b5745885c592c498126a3e428bebbaa2e3017
Susumu <sumura80@gmail.com>
2018-10-22 22:58:31 +0900

    Modify create.js.erb

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,19 +1,17 @@
 class VotesController < ApplicationController
  	def create
+    @post = Post.find_by(id: params[:id])
 		#create.js.erbにpost情報を渡したいためインスタンス変数を利用。
 	  vote = Vote.create(user_id: current_user.id, comment_id: params[:comment_id])
     @comment = vote.comment
   end
 
-		
-
   def destroy
-
+    @post = Post.find_by(id: params[:id])
 		#create.js.erbにpost情報を渡したいためインスタンス変数を利用。
 		vote = current_user.votes.find_by(comment_id: params[:comment_id])
     @comment = vote.comment
     vote.destroy
   end
-
-  
+ 
 end
