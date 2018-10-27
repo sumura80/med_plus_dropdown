d79aaef0d45421f034a3e6ea63df81e8bd849e39
Susumu <sumura80@gmail.com>
2018-10-27 09:53:27 +0900

    add vote button

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,6 +4,9 @@ class VotesController < ApplicationController
 		#create.js.erbにpost情報を渡したいためインスタンス変数を利用。
 	  vote = Vote.create(user_id: current_user.id, comment_id: params[:comment_id])
     @comment = vote.comment
+    @vote_class = ".vote-#{@comment.id}"
+    # class名を動的にしたため、該当のプロパティを指定しjsファイルにつなげる
+
   end
 
   def destroy
@@ -11,6 +14,7 @@ class VotesController < ApplicationController
 		#create.js.erbにpost情報を渡したいためインスタンス変数を利用。
 		vote = current_user.votes.find_by(comment_id: params[:comment_id])
     @comment = vote.comment
+    @vote_class = ".vote-#{@comment.id}"
     vote.destroy
   end
  
