b97d2ea476590c7aa742326a1c941123df0615da
Susumu <sumura80@gmail.com>
2018-11-06 11:09:12 +0900

    Add logic deletion

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,10 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-	def soft_destroy
-		@post.destroy.deleted_at
-  	redirect_to root_path
-	end
-
-	
-
   self.abstract_class = true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User < ApplicationRecord
   has_many :likes
   has_many :votes
   mount_uploader :user_image, ImageUploader
+
+  #usernameを確実に入力させるためのvalidates usernameがuniqueであることと、
+  #文字数が4から20文字であることを設定出来ます。
+  #(case_sensitive: :falssは大文字小文字の区別をしないということ）
+  validates :username,
+   uniqueness: { case_sensitive: :true },
+  length: { minimum: 4, maximum: 20 }
   
  #↓論理削除をするためのparanoiaの導入
   acts_as_paranoid
